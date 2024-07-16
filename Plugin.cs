a261da36e962a7da897328b6a4a7a6c41b1a7e7c
TheHeadPatCat <tomfegert@web.de>
2024-07-16 12:47:42 +0200

    Update Version

--- Plugin.cs
+++ Plugin.cs
@@ -23,8 +23,8 @@ public sealed class Plugin : IDalamudPlugin
     private const string Failsafe = "/red";
     private const string OpenConfigFolder = "/wolfolder";
 
-    public const string currentVersion = "0.2.5.5";
-    public const string randomKey = "0100001011100101001010000111101011101010000111001110101011101111110101110111100011";
+    public const string currentVersion = "0.2.5.6";
+    public const string randomKey = "0100001011100111000010000111111011101010000111001110101011101101110101110111100011";
     public string? ConfigurationDirectoryPath { get; set; }
 
 
